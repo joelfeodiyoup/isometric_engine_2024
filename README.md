9ea910e3f6c1f4ed0f3cf1083833dffe277d2e14
joel <joel.w.mundy@gmail.com>
2024-05-09 14:50:08 +0200

    remove some junk from react app entry point

--- README.md
+++ README.md
@@ -72,3 +72,8 @@ some tasks I could could do...
 - build and animate a vehicle in blender to go into the game
 - fix the thing with passing around Grid
 - ~~probably make the Draw class a property contained in the abstract Canvas class? Then we never need to worry about passing a canvas to Draw methods.~~
+
+- add a back end to do something
+- add a graphql server
+- add authentication / authorization
+- add rate limiting / traffic filtering for back end
--- ui/app.tsx
+++ ui/app.tsx
@@ -1,22 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import {store} from '../state/app/store';
 import { Provider } from 'react-redux';
-import { useAppDispatch, useAppSelector } from '../state/app/hooks';
-import { increment } from '../state/features/counter/counterSlice';
 import { toggle } from '../state/features/highlightType/highlightTypeSlice';
 import { TopNav } from './TopNav';
 import { Terrain } from './Terrain';
 
 const App = () => {
-  const count = useAppSelector((state) => state.counter.value);
-  const t = useAppSelector((state) => state.highlightType.value);
-  const dispatch = useAppDispatch();
   return (<>
-    <p>here is my app</p>
-    <pre>count: {count}</pre>
-    <button onClick={() => dispatch(increment())}>add</button>
-    <pre>t: {t}</pre>
-    <button onClick={() => dispatch(toggle())}>toggle</button>
     <TopNav></TopNav>
     <Terrain />
   </>)
@@ -34,4 +24,3 @@ root.render(
 );
 document.addEventListener("DOMContentLoaded", () => {
 })
-
