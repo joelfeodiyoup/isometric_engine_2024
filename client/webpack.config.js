c60ac83e470c8137b785c395dc6571e212ce71ff
joel <joel.w.mundy@gmail.com>
2024-05-10 16:44:49 +0200

    completely separate client and server builds to avoid weirdness and also adjust module resolution on server to get around annoying node issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: [/*'/client/render/game-render.ts', */'/client/ui/app.tsx'],
+  entry: [/*'/client/render/game-render.ts', */'/ui/app.tsx'],
   module: {
     rules: [
       {
